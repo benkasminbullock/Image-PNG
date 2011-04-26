3aff4c644872bd960f6b3b12306225d9f628b8cc
Ben Bullock <benkasminbullock@gmail.com>
2011-04-26 23:42:27 +0900

    Fixed bug in error handling.

--- t/private-chunks.pl
+++ t/private-chunks.pl
@@ -5,10 +5,15 @@ use autodie;
 use lib '/home/ben/projects/Image-PNG/blib/lib';
 use lib '/home/ben/projects/Image-PNG/blib/arch';
 use Image::PNG::Libpng ':all';
+use Image::PNG::Const ':all';
 my $file = 'banner_green_servers.png';
 open my $input, "<:raw", $file;
 my $png = create_read_struct ();
+print "OK 0\n";
 set_verbosity ($png, 1);
+print "OK -1\n";
+#set_keep_unknown_chunks ($png, 3, [qw/mkBT prVW/]);
+set_keep_unknown_chunks ($png, 3);
 print "OK 1\n";
 init_io ($png, $input);
 print "OK 2\n";
@@ -19,6 +24,6 @@ print "OK 4\n";
 my $chunks = get_unknown_chunks ($png);
 print "OK 5\n";
 for my $chunk (@$chunks) {
-    print $chunk->{name}, length $chunk->{data}, "\n";
+    print "name: ", $chunk->{name}, " length: ", length $chunk->{data}, "\n";
 }
 print "OK 6\n";
