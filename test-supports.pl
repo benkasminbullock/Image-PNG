d13ac12545f15f60b52bba79a55c453b105f5fab
Ben Bullock <benkasminbullock@gmail.com>
2013-02-15 20:26:50 +0900

    Change read_file to return a new $png. Tidy up files.

--- test-supports.pl
+++
@@ -1,8 +0,0 @@
-#!/home/ben/software/install/bin/perl
-use warnings;
-use strict;
-use lib 'blib/lib';
-use lib 'blib/arch';
-use Image::PNG::Libpng 'supports';
-print supports ('iTXt');
-print "\n";
